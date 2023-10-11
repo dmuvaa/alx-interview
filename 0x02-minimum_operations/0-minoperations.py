d39fd81f6d4c6048bca34640d77ce21f6cc2fdaf
Dennis Muvaa <dmuvaa70@gmail.com>
2023-10-11 21:44:56 +0300

    Minimum Operations method that calculates the fewest number of operations needed to result in exactly n H characters in the file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""Create a Function for Min Operations"""
+
+
+def minOperations(n):
+    """Function that calculates the fewest number of operations"""
+    if n <= 1:
+        return 0
+
+    res = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            res += divisor
+            n = n // divisor
+        
+        else:
+            divisor += 1
+
+    return res
