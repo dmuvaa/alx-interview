8109d261c5e239fea67c4e7f0e51cdc132822470
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-23 18:15:17 +0300

    Method that determines the fewest number of coins needed to meet a given amount

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,23 +2,18 @@
 
 """Create a Function"""
 
-
 def makeChange(coins, total):
     """Func that determines the fewest number of coins
     needed to meet a given amount
     """
     if total <= 0:
         return 0
-    
-    coins.sort(reverse=True)
 
-    minCoins = [float('inf')] * (total + 1)
+    minCoins = [total + 1] * (total + 1)
     minCoins[0] = 0
 
     for coin in coins:
-        if coin > total:
-            continue
         for i in range(coin, total + 1):
             minCoins[i] = min(minCoins[i], minCoins[i - coin] + 1)
 
-    return -1 if minCoins[total] == float('inf') else minCoins[total]
+    return -1 if minCoins[total] > total else minCoins[total]
