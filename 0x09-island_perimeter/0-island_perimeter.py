29e2adf0826bf98cbbec7de9aa861baed778b83d
Dennis Muvaa <dmuvaa70@gmail.com>
2023-11-30 12:05:12 +0300

    Function for Island Perimeter Calculation

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,7 +4,7 @@
 
 def island_perimeter(grid):
     """Function that returns the perimeter of the island"""
-    perimeter = 0;
+    perimeter = 0
 
     rows, cols = len(grid), len(grid[0])
 
@@ -13,14 +13,14 @@ def island_perimeter(grid):
         return r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0
     for r in range(rows):
         for c in range(cols):
-            if grid[r][c] == 1: """if cell is land, check 4 directions"""
-            if is_water_or_outside(r-1, c):
-                perimeter += 1
-            if is_water_or_outside(r+1, c):
-                perimeter += 1
-            if is_water_or_outside(r, c-1):
-                perimeter += 1
-            if is_water_or_outside(r, c+1):
-                perimeter += 1
+            if grid[r][c] == 1:
+                if is_water_or_outside(r-1, c):
+                    perimeter += 1
+                if is_water_or_outside(r+1, c):
+                    perimeter += 1
+                if is_water_or_outside(r, c-1):
+                    perimeter += 1
+                if is_water_or_outside(r, c+1):
+                    perimeter += 1
 
     return perimeter
